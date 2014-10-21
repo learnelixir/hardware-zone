56d655e57b5bf4c27e6003f5d6ecafe5564b5353
James Huynh <james@rubify.com>
2014-10-22 07:30:42 +0800

    update repo password

--- web/models/repo.ex
+++ web/models/repo.ex
@@ -2,7 +2,7 @@ defmodule HardwareZone.Repo do
   use Ecto.Repo, adapter: Ecto.Adapters.Postgres
 
   def conf do
-    parse_url "ecto://gnukdiprjdibvv:@ec2-54-197-250-40.compute-1.amazonaws.com/d9v5t7afd4osse"
+    parse_url "ecto://gnukdiprjdibvv:MObPUSRcFM6omOjNZPsAp6OLFU@ec2-54-197-250-40.compute-1.amazonaws.com/d9v5t7afd4osse"
   end
 
   def priv do
